4e0b11ddd9c76bb64c6ee482c10925fcb6c9e764
Artem <91727914+Amanaezi@users.noreply.github.com>
2022-11-04 22:52:05 +0200

    Добавьте файлы проекта.

---
+++ Pendulum/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VectorLib;
+
+namespace Pendulum
+{
+    public partial class Form1 : Form
+    {
+        PictureBall pb;
+        VectorLib.Point p1, p2, p3, p4;
+        Graphics g;
+        public Form1()
+        {
+            InitializeComponent();
+            p1 = new VectorLib.Point(-10, ClientSize.Height);
+            p2 = new VectorLib.Point(Width, ClientSize.Height / 2);
+            p3 = new VectorLib.Point(Width, -10);
+            p4 = new VectorLib.Point(-10, -10);
+            pb = new PictureBall(Color.Green, this);
+            pb.Ball.V = new Vector(200, 300);
+            g = CreateGraphics();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pb.MoveWithBounce(timer1.Interval / 1000.0, p1, p2, p3, p4, p1);
+            pb.Update();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            g.DrawLine(Pens.Red, (int)p1.X, (int)p1.Y, (int)p2.X, (int)p2.Y);
+            timer1.Start();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Pendulum/PictureBall.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using VectorLib;
+using Point = System.Drawing.Point;
+
+namespace Pendulum
+{
+    public class PictureBall
+    {
+        public Ball Ball { get; set; }
+        public PictureBox Pb { get; set; }
+        public double Scale { get; set; }
+
+        private Control parent;
+
+        
+
+        public PictureBall(PictureBox pb, double scale)
+        {
+            parent = pb.Parent;
+            Pb = pb;
+            Scale = scale;
+            Ball = new Ball()
+            {
+                R = new Vector((pb.Left + pb.Width / 2.0) / Scale, (pb.Top + pb.Height) / 2.0 / Scale),
+                Radius = (pb.Height + pb.Width) / 4.0 / Scale
+            };    
+        }
+
+        public PictureBall(Color color, Control Parent, int size = 20, double scale = 1) :
+                this(CreatePb(color, Parent, size), scale)
+        { }
+
+        public static PictureBox CreatePb(Control parent, int size = 20)
+        {
+            PictureBox pb = new PictureBox();
+            pb.Size = new Size(size, size);
+            pb.SizeMode = PictureBoxSizeMode.Zoom;
+            parent.Controls.Add(pb);
+            return pb;
+        }
+
+        public static PictureBox CreatePb(Color color, Control parent, int size = 20)
+        {
+            PictureBox pb = CreatePb(parent, size);
+            pb.BackColor = color;
+            return pb;
+        }
+
+        public void MoveWithBounce(double dt, params VectorLib.Point[] points)
+        {
+            Ball.Bounce(points);
+            Ball.Mp.Move(dt);
+            Update();
+        }
+        public void Update()
+        {
+            Pb.Location = new Point((int)(Ball.R.X * Scale) - Pb.Width / 2, (int)(Ball.R.Y * Scale) - Pb.Height / 2);
+            parent.Update();
+        }
+
+    }
+}
